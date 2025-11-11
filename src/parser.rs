9d1cec67c6ff14d9335f35155abae5a630aa36bf
Nathan Adams <dinnerbone@dinnerbone.com>
2025-11-11 22:11:16 +0100

    Move parser error to error.rs

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,11 @@
 use crate::lexer::tokens::{Token, TokenKind};
-use annotate_snippets::renderer::DecorStyle;
-use annotate_snippets::{AnnotationKind, Renderer};
 use winnow::combinator::opt;
 use winnow::error::{ContextError, ErrMode, ParseError};
 use winnow::stream::TokenSlice;
 use winnow::{ModalResult, Parser};
 
 mod document;
+mod error;
 mod expression;
 mod statement;
 #[cfg(test)]
@@ -15,45 +14,7 @@ mod tests;
 pub(crate) type Tokens<'i> = TokenSlice<'i, Token<'i>>;
 
 use crate::parser::document::Document;
-
-#[derive(Debug)]
-pub struct ActionScriptError<'a> {
-    filename: &'a str,
-    source: &'a str,
-    error: ParseError<Tokens<'a>, ContextError>,
-}
-
-impl<'a> ActionScriptError<'a> {
-    pub(crate) fn from_parse(
-        filename: &'a str,
-        source: &'a str,
-        error: ParseError<Tokens<'a>, ContextError>,
-    ) -> Self {
-        Self {
-            filename,
-            source,
-            error,
-        }
-    }
-}
-
-impl std::fmt::Display for ActionScriptError<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let tokens = self.error.input();
-        let mut source = annotate_snippets::Snippet::source(self.source).path(self.filename);
-        if let Some(bad_token) = tokens.get(self.error.offset()) {
-            source = source
-                .annotation(AnnotationKind::Primary.span(bad_token.span.start..bad_token.span.end));
-        }
-        let report = &[annotate_snippets::Level::ERROR
-            .primary_title(self.error.inner().to_string())
-            .element(source)];
-        let renderer = Renderer::styled().decor_style(DecorStyle::Unicode);
-        renderer.render(report).fmt(f)
-    }
-}
-
-impl std::error::Error for ActionScriptError<'_> {}
+pub use crate::parser::error::ActionScriptError;
 
 pub fn parse_document<'a>(
     source: &'a [Token],
