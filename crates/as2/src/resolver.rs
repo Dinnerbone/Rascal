ef56abff7a040599c55d3c2a17b2047b5f6cb320
Nathan Adams <dinnerbone@dinnerbone.com>
2025-12-03 20:33:33 +0100

    Introduce ModuleContext for resolving AST->HIL

--- crates/as2/src/resolver.rs
+++ crates/as2/src/resolver.rs
@@ -1,22 +1,44 @@
 use crate::ast;
 use crate::hir;
+use std::marker::PhantomData;
 
-pub fn resolve_hir(ast: ast::Document) -> hir::Document {
-    hir::Document {
-        statements: resolve_statement_vec(ast.statements),
+struct ModuleContext<'src>(PhantomData<&'src ()>);
+
+impl<'src> ModuleContext<'src> {
+    fn new() -> Self {
+        Self(PhantomData)
     }
 }
 
-fn resolve_statement_vec(input: Vec<ast::StatementKind>) -> Vec<hir::StatementKind> {
-    input.into_iter().map(resolve_statement).collect()
+pub fn resolve_hir(ast: ast::Document) -> hir::Document {
+    let mut context = ModuleContext::new();
+    hir::Document {
+        statements: resolve_statement_vec(&mut context, ast.statements),
+    }
+}
+
+fn resolve_statement_vec<'src>(
+    context: &mut ModuleContext<'src>,
+    input: Vec<ast::StatementKind<'src>>,
+) -> Vec<hir::StatementKind<'src>> {
+    input
+        .into_iter()
+        .map(|statement| resolve_statement(context, statement))
+        .collect()
 }
 
 #[expect(clippy::boxed_local)]
-fn resolve_statement_box(input: Box<ast::StatementKind>) -> Box<hir::StatementKind> {
-    Box::new(resolve_statement(*input))
+fn resolve_statement_box<'src>(
+    context: &mut ModuleContext<'src>,
+    input: Box<ast::StatementKind<'src>>,
+) -> Box<hir::StatementKind<'src>> {
+    Box::new(resolve_statement(context, *input))
 }
 
-fn resolve_statement(input: ast::StatementKind) -> hir::StatementKind {
+fn resolve_statement<'src>(
+    context: &mut ModuleContext<'src>,
+    input: ast::StatementKind<'src>,
+) -> hir::StatementKind<'src> {
     match input {
         ast::StatementKind::Declare(declarations) => hir::StatementKind::Declare(
             declarations
@@ -24,67 +46,79 @@ fn resolve_statement(input: ast::StatementKind) -> hir::StatementKind {
                 .map(|d| hir::Declaration {
                     name: d.name,
                     type_name: d.type_name,
-                    value: d.value.map(resolve_expr),
+                    value: d.value.map(|expr| resolve_expr(context, expr)),
                 })
                 .collect(),
         ),
-        ast::StatementKind::Return(values) => hir::StatementKind::Return(resolve_expr_vec(values)),
-        ast::StatementKind::Throw(values) => hir::StatementKind::Throw(resolve_expr_vec(values)),
-        ast::StatementKind::Expr(expr) => hir::StatementKind::Expr(resolve_expr(expr)),
+        ast::StatementKind::Return(values) => {
+            hir::StatementKind::Return(resolve_expr_vec(context, values))
+        }
+        ast::StatementKind::Throw(values) => {
+            hir::StatementKind::Throw(resolve_expr_vec(context, values))
+        }
+        ast::StatementKind::Expr(expr) => hir::StatementKind::Expr(resolve_expr(context, expr)),
         ast::StatementKind::Block(statements) => {
-            hir::StatementKind::Block(resolve_statement_vec(statements))
+            hir::StatementKind::Block(resolve_statement_vec(context, statements))
         }
         ast::StatementKind::ForIn { condition, body } => hir::StatementKind::ForIn {
-            condition: resolve_for_condition(condition),
-            body: resolve_statement_box(body),
+            condition: resolve_for_condition(context, condition),
+            body: resolve_statement_box(context, body),
         },
         ast::StatementKind::While { condition, body } => hir::StatementKind::While {
-            condition: resolve_expr(condition),
-            body: resolve_statement_box(body),
+            condition: resolve_expr(context, condition),
+            body: resolve_statement_box(context, body),
         },
         ast::StatementKind::If { condition, yes, no } => hir::StatementKind::If {
-            condition: resolve_expr(condition),
-            yes: resolve_statement_box(yes),
-            no: no.map(resolve_statement_box),
+            condition: resolve_expr(context, condition),
+            yes: resolve_statement_box(context, yes),
+            no: no.map(|statement| resolve_statement_box(context, statement)),
         },
         ast::StatementKind::Break => hir::StatementKind::Break,
         ast::StatementKind::Continue => hir::StatementKind::Continue,
         ast::StatementKind::Try(try_catch) => hir::StatementKind::Try(hir::TryCatch {
-            try_body: resolve_statement_vec(try_catch.try_body),
-            catch_all: try_catch.catch_all.map(resolve_catch),
+            try_body: resolve_statement_vec(context, try_catch.try_body),
+            catch_all: try_catch
+                .catch_all
+                .map(|catch| resolve_catch(context, catch)),
             typed_catches: try_catch
                 .typed_catches
                 .into_iter()
-                .map(|(type_name, catch)| (type_name, resolve_catch(catch)))
+                .map(|(type_name, catch)| (type_name, resolve_catch(context, catch)))
                 .collect(),
-            finally: resolve_statement_vec(try_catch.finally),
+            finally: resolve_statement_vec(context, try_catch.finally),
         }),
         ast::StatementKind::WaitForFrame {
             frame,
             scene,
             if_loaded,
         } => hir::StatementKind::WaitForFrame {
-            frame: resolve_expr(frame),
-            scene: scene.map(resolve_expr),
-            if_loaded: resolve_statement_box(if_loaded),
+            frame: resolve_expr(context, frame),
+            scene: scene.map(|expr| resolve_expr(context, expr)),
+            if_loaded: resolve_statement_box(context, if_loaded),
         },
         ast::StatementKind::TellTarget { target, body } => hir::StatementKind::TellTarget {
-            target: resolve_expr(target),
-            body: resolve_statement_box(body),
+            target: resolve_expr(context, target),
+            body: resolve_statement_box(context, body),
         },
         ast::StatementKind::InlinePCode(pcode) => hir::StatementKind::InlinePCode(pcode),
         ast::StatementKind::With { target, body } => hir::StatementKind::With {
-            target: resolve_expr(target),
-            body: resolve_statement_box(body),
+            target: resolve_expr(context, target),
+            body: resolve_statement_box(context, body),
         },
         ast::StatementKind::Switch { target, elements } => hir::StatementKind::Switch {
-            target: resolve_expr(target),
-            elements: elements.into_iter().map(resolve_switch_element).collect(),
+            target: resolve_expr(context, target),
+            elements: elements
+                .into_iter()
+                .map(|element| resolve_switch_element(context, element))
+                .collect(),
         },
     }
 }
 
-fn resolve_for_condition(input: ast::ForCondition) -> hir::ForCondition {
+fn resolve_for_condition<'src>(
+    context: &mut ModuleContext<'src>,
+    input: ast::ForCondition<'src>,
+) -> hir::ForCondition<'src> {
     match input {
         ast::ForCondition::Enumerate {
             variable,
@@ -93,100 +127,141 @@ fn resolve_for_condition(input: ast::ForCondition) -> hir::ForCondition {
         } => hir::ForCondition::Enumerate {
             variable,
             declare,
-            object: resolve_expr(object),
+            object: resolve_expr(context, object),
         },
         ast::ForCondition::Classic {
             initialize,
             condition,
             update,
         } => hir::ForCondition::Classic {
-            initialize: initialize.map(resolve_statement_box),
-            condition: resolve_expr_vec(condition),
-            update: resolve_expr_vec(update),
+            initialize: initialize.map(|statement| resolve_statement_box(context, statement)),
+            condition: resolve_expr_vec(context, condition),
+            update: resolve_expr_vec(context, update),
         },
     }
 }
 
-fn resolve_catch(input: ast::Catch) -> hir::Catch {
+fn resolve_catch<'src>(
+    context: &mut ModuleContext<'src>,
+    input: ast::Catch<'src>,
+) -> hir::Catch<'src> {
     hir::Catch {
         name: input.name,
-        body: resolve_statement_vec(input.body),
+        body: resolve_statement_vec(context, input.body),
     }
 }
 
-fn resolve_switch_element(input: ast::SwitchElement) -> hir::SwitchElement {
+fn resolve_switch_element<'src>(
+    context: &mut ModuleContext<'src>,
+    input: ast::SwitchElement<'src>,
+) -> hir::SwitchElement<'src> {
     match input {
-        ast::SwitchElement::Case(expr) => hir::SwitchElement::Case(resolve_expr(expr)),
+        ast::SwitchElement::Case(expr) => hir::SwitchElement::Case(resolve_expr(context, expr)),
         ast::SwitchElement::Default => hir::SwitchElement::Default,
         ast::SwitchElement::Statement(statement) => {
-            hir::SwitchElement::Statement(resolve_statement(statement))
+            hir::SwitchElement::Statement(resolve_statement(context, statement))
         }
     }
 }
 
 #[expect(clippy::boxed_local)]
-fn resolve_expr_box(input: Box<ast::Expr>) -> Box<hir::Expr> {
-    Box::new(resolve_expr(*input))
+fn resolve_expr_box<'src>(
+    context: &mut ModuleContext<'src>,
+    input: Box<ast::Expr<'src>>,
+) -> Box<hir::Expr<'src>> {
+    Box::new(resolve_expr(context, *input))
 }
 
-fn resolve_expr_vec(input: Vec<ast::Expr>) -> Vec<hir::Expr> {
-    input.into_iter().map(resolve_expr).collect()
+fn resolve_expr_vec<'src>(
+    context: &mut ModuleContext<'src>,
+    input: Vec<ast::Expr<'src>>,
+) -> Vec<hir::Expr<'src>> {
+    input
+        .into_iter()
+        .map(|expr| resolve_expr(context, expr))
+        .collect()
 }
 
-fn resolve_expr(input: ast::Expr) -> hir::Expr {
+fn resolve_expr<'src>(
+    context: &mut ModuleContext<'src>,
+    input: ast::Expr<'src>,
+) -> hir::Expr<'src> {
     let result = match input.value {
         ast::ExprKind::Constant(value) => hir::ExprKind::Constant(value),
         ast::ExprKind::Call { name, args } => hir::ExprKind::Call {
-            name: resolve_expr_box(name),
-            args: resolve_expr_vec(args),
+            name: resolve_expr_box(context, name),
+            args: resolve_expr_vec(context, args),
         },
         ast::ExprKind::New { name, args } => hir::ExprKind::New {
-            name: resolve_expr_box(name),
-            args: resolve_expr_vec(args),
+            name: resolve_expr_box(context, name),
+            args: resolve_expr_vec(context, args),
         },
-        ast::ExprKind::BinaryOperator(op, left, right) => {
-            hir::ExprKind::BinaryOperator(op, resolve_expr_box(left), resolve_expr_box(right))
-        }
+        ast::ExprKind::BinaryOperator(op, left, right) => hir::ExprKind::BinaryOperator(
+            op,
+            resolve_expr_box(context, left),
+            resolve_expr_box(context, right),
+        ),
         ast::ExprKind::UnaryOperator(op, value) => {
-            hir::ExprKind::UnaryOperator(op, resolve_expr_box(value))
+            hir::ExprKind::UnaryOperator(op, resolve_expr_box(context, value))
         }
-        ast::ExprKind::Parenthesis(expr) => return resolve_expr(*expr),
+        ast::ExprKind::Parenthesis(expr) => return resolve_expr(context, *expr),
         ast::ExprKind::Ternary { condition, yes, no } => hir::ExprKind::Ternary {
-            condition: resolve_expr_box(condition),
-            yes: resolve_expr_box(yes),
-            no: resolve_expr_box(no),
+            condition: resolve_expr_box(context, condition),
+            yes: resolve_expr_box(context, yes),
+            no: resolve_expr_box(context, no),
         },
         ast::ExprKind::InitObject(values) => hir::ExprKind::InitObject(
             values
                 .into_iter()
-                .map(|(name, value)| (name, resolve_expr(value)))
+                .map(|(name, value)| (name, resolve_expr(context, value)))
                 .collect(),
         ),
-        ast::ExprKind::InitArray(values) => {
-            hir::ExprKind::InitArray(values.into_iter().map(resolve_expr).collect())
+        ast::ExprKind::InitArray(values) => hir::ExprKind::InitArray(
+            values
+                .into_iter()
+                .map(|expr| resolve_expr(context, expr))
+                .collect(),
+        ),
+        ast::ExprKind::Field(object, field) => hir::ExprKind::Field(
+            resolve_expr_box(context, object),
+            resolve_expr_box(context, field),
+        ),
+        ast::ExprKind::TypeOf(values) => hir::ExprKind::TypeOf(
+            values
+                .into_iter()
+                .map(|expr| resolve_expr(context, expr))
+                .collect(),
+        ),
+        ast::ExprKind::Delete(values) => hir::ExprKind::Delete(
+            values
+                .into_iter()
+                .map(|expr| resolve_expr(context, expr))
+                .collect(),
+        ),
+        ast::ExprKind::Void(values) => hir::ExprKind::Void(
+            values
+                .into_iter()
+                .map(|expr| resolve_expr(context, expr))
+                .collect(),
+        ),
+        ast::ExprKind::Function(function) => {
+            hir::ExprKind::Function(resolve_function(context, function))
         }
-        ast::ExprKind::Field(object, field) => {
-            hir::ExprKind::Field(resolve_expr_box(object), resolve_expr_box(field))
+        ast::ExprKind::GetVariable(name) => {
+            hir::ExprKind::GetVariable(resolve_expr_box(context, name))
         }
-        ast::ExprKind::TypeOf(values) => {
-            hir::ExprKind::TypeOf(values.into_iter().map(resolve_expr).collect())
-        }
-        ast::ExprKind::Delete(values) => {
-            hir::ExprKind::Delete(values.into_iter().map(resolve_expr).collect())
-        }
-        ast::ExprKind::Void(values) => {
-            hir::ExprKind::Void(values.into_iter().map(resolve_expr).collect())
-        }
-        ast::ExprKind::Function(function) => hir::ExprKind::Function(resolve_function(function)),
-        ast::ExprKind::GetVariable(name) => hir::ExprKind::GetVariable(resolve_expr_box(name)),
-        ast::ExprKind::SetVariable(name, value) => {
-            hir::ExprKind::SetVariable(resolve_expr_box(name), resolve_expr_box(value))
-        }
+        ast::ExprKind::SetVariable(name, value) => hir::ExprKind::SetVariable(
+            resolve_expr_box(context, name),
+            resolve_expr_box(context, value),
+        ),
     };
     hir::Expr::new(input.span, result)
 }
 
-fn resolve_function(input: ast::Function) -> hir::Function {
+fn resolve_function<'src>(
+    context: &mut ModuleContext<'src>,
+    input: ast::Function<'src>,
+) -> hir::Function<'src> {
     hir::Function {
         name: input.name,
         args: input
@@ -197,6 +272,6 @@ fn resolve_function(input: ast::Function) -> hir::Function {
                 type_name: arg.type_name,
             })
             .collect(),
-        body: resolve_statement_vec(input.body),
+        body: resolve_statement_vec(context, input.body),
     }
 }
