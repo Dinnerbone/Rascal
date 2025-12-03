364d5e6da989090a2c50b248d4e45014c3e786df
Nathan Adams <dinnerbone@dinnerbone.com>
2025-12-03 17:19:49 +0100

    Use ActionScript::to_ast in parsing snapshots

--- crates/as2/src/parser/tests.rs
+++ crates/as2/src/parser/tests.rs
@@ -1,6 +1,4 @@
-use crate::lexer::Lexer;
 use crate::lexer::tokens::{Token, TokenKind};
-use crate::parser::parse_document;
 use rascal_common::span::Span;
 
 pub(crate) fn build_tokens<'i>(spec: &'i [(TokenKind, &'i str)]) -> Vec<Token<'i>> {
@@ -13,8 +11,8 @@ pub(crate) fn build_tokens<'i>(spec: &'i [(TokenKind, &'i str)]) -> Vec<Token<'i
 fn test_all_samples() {
     insta::glob!("../../../../samples/as2", "**/*.as", |path| {
         let src = std::fs::read_to_string(path).expect("failed to read sample");
-        let tokens = Lexer::new(&src).into_vec();
-        let parsed = parse_document(&tokens).map_err(|e| format!("{e:#?}"));
+        let actionscript = crate::ActionScript::new("test.as", &src);
+        let parsed = actionscript.to_ast().map_err(|e| format!("{e:#?}"));
         insta::assert_yaml_snapshot!(parsed);
     });
 }
