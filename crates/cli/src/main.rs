222f3689e52fa5c42889c4aad29880dc1d806fec
Nathan Adams <dinnerbone@dinnerbone.com>
2026-04-20 22:52:19 +0200

    Add -v (or --swf-version) to cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -12,26 +12,29 @@ struct Opt {
     /// Input source file to compile.
     #[arg(name = "FILE")]
     src: PathBuf,
+
+    /// SWF version to use.
+    #[arg(short = 'v', long, default_value_t = 15)]
+    swf_version: u8,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::parse();
     let filename = opt.src.to_string_lossy();
-    let swf_version = 15;
     let pcode = if filename.ends_with(".as") {
         let root = opt.src.parent().unwrap_or_else(|| Path::new("."));
         let provider = FileSystemSourceProvider::with_root(root.to_owned());
         let mut builder = ProgramBuilder::new(provider);
         builder.add_script(&opt.src.file_name().unwrap().to_string_lossy());
         let parsed = builder.build().unwrap_or_else(|e| panic!("{}", e));
-        hir_to_pcode(&parsed, swf_version)
+        hir_to_pcode(&parsed, opt.swf_version)
     } else {
         let src = fs::read_to_string(&opt.src)?;
         let pcode = PCode::new(&filename, &src);
         CompiledProgram {
             initializer: Some(pcode.to_actions().unwrap_or_else(|e| panic!("{}", e))),
             extra_modules: vec![],
-            swf_version,
+            swf_version: opt.swf_version,
         }
     };
     let swf = pcode_to_swf(&pcode)?;
