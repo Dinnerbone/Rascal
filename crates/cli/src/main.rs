61f0aa899b350bb4984d08f8a24a42fd3bbfce17
Nathan Adams <dinnerbone@dinnerbone.com>
2026-04-22 21:01:30 +0200

    Add `--class` cli argument

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -12,15 +12,31 @@ struct Opt {
     /// A loose script (not class file) to add to the compiled program.
     ///
     /// Multiple scripts may be added to one program.
-    /// Scripts will be executed in order that they are added.
+    /// Scripts will be executed in order that they are added, before any classes are loaded.
+    ///
+    /// It is valid to not specify any scripts - if so, the entry point of the program will be the first
+    /// class (see `--class`) that contains a `static function main()` method.
     #[arg(name = "SCRIPT")]
     script: Vec<PathBuf>,
 
     /// Output file path. This will be overwritten if it already exists.
     ///
-    /// If not specified, the first script path (with ".swf" instead of ".as") will be used.
-    #[arg(short, long)]
+    /// If not specified:
+    /// - If there are any scripts (see `SCRIPT`) specified, the first one will be used to determine the output path.
+    /// - `output.swf` will be used if all else fails.
+    #[arg(short, long, verbatim_doc_comment)]
     output: Option<PathBuf>,
+
+    /// A name of a class to be added to the compiled program.
+    ///
+    /// This should be the canonical name of a class (e.g. `Foo` or `foo.bar.Baz`) which should be found in the current directory
+    /// (e.g. at `Foo.as` or `foo/bar/Baz.as`).
+    ///
+    /// All classes will be loaded after any scripts (see `SCRIPT`).
+    /// If no scripts are specified, one (and only one) class is expected to have an entry point in
+    /// the form of a `static function main()` method.
+    #[arg(short, long)]
+    class: Vec<String>,
 
     /// SWF version to use.
     #[arg(short = 'v', long, default_value_t = 15)]
@@ -34,21 +50,23 @@ struct Opt {
 fn main() -> Result<()> {
     let opt = Opt::parse();
 
-    let Some(first_script) = opt.script.first() else {
-        return Err(anyhow::anyhow!("No scripts specified (see --help)."));
-    };
-
-    let root = first_script.parent().unwrap_or_else(|| Path::new("."));
+    let root = Path::new(".");
     let provider = FileSystemSourceProvider::with_root(root.to_owned());
     let mut builder = ProgramBuilder::new(provider);
+
     for src in &opt.script {
         builder.add_script(&src.file_name().unwrap().to_string_lossy());
     }
+    for class in &opt.class {
+        builder.add_class(class);
+    }
+
     let parsed = builder.build().unwrap_or_else(|e| panic!("{}", e));
     let pcode = hir_to_pcode(&parsed, opt.swf_version);
     let output_path = opt
         .output
-        .unwrap_or_else(|| first_script.with_extension("swf"));
+        .or_else(|| opt.script.first().map(|s| s.with_extension("swf")))
+        .unwrap_or_else(|| PathBuf::from("output.swf"));
     let swf = pcode_to_swf(&pcode, opt.frame_rate)?;
     fs::write(&output_path, swf)?;
     Ok(())
