818cbd11d6f523199edf087deff5b9c1680b0c50
Nathan Adams <dinnerbone@dinnerbone.com>
2026-04-22 21:19:04 +0200

    Allow multiple roots in FileSystemSourceProvider

--- crates/as2/src/program.rs
+++ crates/as2/src/program.rs
@@ -15,22 +15,41 @@ pub trait SourceProvider {
 }
 
 pub struct FileSystemSourceProvider {
-    root: PathBuf,
+    roots: Vec<PathBuf>,
 }
 
 impl FileSystemSourceProvider {
     pub fn with_root(root: PathBuf) -> Self {
-        Self { root }
+        Self::with_roots(vec![root])
+    }
+
+    pub fn with_roots(roots: Vec<PathBuf>) -> Self {
+        Self { roots }
     }
 }
 
 impl SourceProvider for FileSystemSourceProvider {
     fn load(&self, path: &str) -> Result<String, std::io::Error> {
-        std::fs::read_to_string(self.root.join(path))
+        for root in &self.roots {
+            let actual_path = root.join(path);
+            if actual_path.is_file() {
+                return std::fs::read_to_string(actual_path);
+            }
+        }
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "File not found",
+        ))
     }
 
     fn is_file(&self, path: &str) -> bool {
-        self.root.join(path).is_file()
+        for root in &self.roots {
+            let actual_path = root.join(path);
+            if actual_path.is_file() {
+                return true;
+            }
+        }
+        false
     }
 }
 
